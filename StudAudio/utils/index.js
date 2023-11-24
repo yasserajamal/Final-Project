578248fc667aac14ff749f682a9f361d5e904a88
“Blain <blain@stanford.edu>
2023-11-24 00:18:02 -0800

    Initial speech-to-text page for notes

--- StudAudio/utils/index.js
+++ StudAudio/utils/index.js
@@ -2,8 +2,9 @@ import SignInScreen from "./Screens/signInScreens/signInScreen";
 import SignUpScreen from "./Screens/signInScreens/signUpScreen";
 import ForgotPassword from "./Screens/signInScreens/forgotPassword";
 import Classes from "./Screens/Classes";
+import Notes from "./Screens/Notes";
 import Help from "./Screens/helpScreen";
-import RecordingsScreen from './Screens/RecordingsScreen'
+import RecordingsScreen from "./Screens/RecordingsScreen";
 import UnderConstructionScreen from "./Screens/UnderConstruction";
 import LogoutScreen from "./Screens/LogoutScreen";
 export {
@@ -11,6 +12,7 @@ export {
   SignUpScreen,
   ForgotPassword,
   Classes,
+  Notes,
   Help,
   RecordingsScreen,
   UnderConstructionScreen,
